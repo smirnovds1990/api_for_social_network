95b614a5c47bef61a16544fc4bcad95035df744b
smirnovds1990 <you@example.comsmirnovds1990@gmail.com>
2023-07-03 20:22:44 +0300

    Add search to FollowViewSet

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets
+from rest_framework import filters, viewsets
 from rest_framework.permissions import IsAuthenticated
 
 from api.permissions import IsAdminOnly, IsAuthorOrReadOnly
 from api.serializers import (
     CommentSerializer, FollowSerializer, GroupSerializer, PostSerializer
 )
-from posts.models import Follow, Group, Post
+from posts.models import Follow, Group, Post, User
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -43,6 +43,12 @@ class FollowViewSet(viewsets.ModelViewSet):
     queryset = Follow.objects.all()
     serializer_class = FollowSerializer
     permission_classes = (IsAuthorOrReadOnly, IsAuthenticated)
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ['user__username']
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.request.user)
+        return user.follower.all()
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
